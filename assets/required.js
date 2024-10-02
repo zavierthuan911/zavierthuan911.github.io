a06060ead4bd573aaa34d79375ed14458cbdbd8f
Jayvee <zavier.thuan911@gmail.com>
2024-10-02 11:22:29 +0700

    final relaesa

--- assets/required.js
+++ assets/required.js
@@ -1,25 +1,26 @@
-function submitForm(e) {
+async function submitForm() {
   var formData = {
     name: document.querySelector("input[name='yourname']").value,
     email: document.querySelector("input[name='youremail']").value,
     message: document.querySelector("textarea[name='yourmsg']").value,
   };
 
-  fetch(
-    "https://script.google.com/macros/s/AKfycbyTjFPQ7bhbmTVipIhF5thtg1hUZFQJMEhWvi6KzxBtWsvWm4ZVsb5L0I3RGM6mM58jAA/exec",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    }
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      alert("Form submitted successfully! File URL: " + data.url);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+  try {
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbwGkPqHjFYV138-W0A4aQNqaNHuopI0kdlpqRJHzfYeHwrRgmJUdB4raS-HPnty4GyuTw/exec",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        mode: "no-cors",
+        body: JSON.stringify(formData),
+      }
+    );
+
+    alert("Message send successfully.");
+  } catch (error) {
+    console.error("Error during fetch request:", error);
+    alert("There was an error submitting the form.");
+  }
 }
