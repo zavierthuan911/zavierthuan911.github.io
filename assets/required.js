0e7c65b8ba5901a15d673ec80ee274d05c4dd842
Jayvee <zavier.thuan911@gmail.com>
2024-10-02 10:46:00 +0700

    contact form updated

---
+++ assets/required.js
@@ -0,0 +1,26 @@
+function submitForm(e) {
+  e.preventDefault();
+  var formData = {
+    name: document.querySelector("input[name='yourname']").value,
+    email: document.querySelector("input[name='youremail']").value,
+    message: document.querySelector("input[name='yourmsg']").value,
+  };
+
+  fetch(
+    "https://script.google.com/macros/s/AKfycbyTjFPQ7bhbmTVipIhF5thtg1hUZFQJMEhWvi6KzxBtWsvWm4ZVsb5L0I3RGM6mM58jAA/exec",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      alert("Form submitted successfully! File URL: " + data.url);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
